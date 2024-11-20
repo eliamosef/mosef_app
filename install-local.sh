823bde446cf7ec7d306d10ff8dfafdb0822c5e6c
eliamosef <118312033+eliamosef@users.noreply.github.com>
2024-11-20 01:01:00 +0100

    Update install-local.sh

--- install-local.sh
+++ install-local.sh
@@ -3,6 +3,7 @@ mkdir -p archived/raw
 mkdir -p archived/staged
 python3 -m venv .venv
 source .venv/bin/activate
+pip install --upgrade pip
 python3 -m pip install -r requirements-local.txt
 export FLASK_APP=webapp/mosef.py
 export FLASK_ENV=development
