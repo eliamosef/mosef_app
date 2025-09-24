8af0b606c6b4a82cc5ba915b022475a43e95f89f
eliamosef <118312033+eliamosef@users.noreply.github.com>
2025-09-24 03:38:59 +0200

    Update run-local.sh

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -3,5 +3,6 @@ source ../.venv/Scripts/activate
 export FLASK_APP=../webapp/mosef.py
 export FLASK_ENV=development
 
-(cd . ; bash launch-local.sh >> ../log/data_pipeline.log) &
-(cd ../webapp/bin; bash launch-local.sh >> ../../log/webapp.log )
+(cd ../webapp/bin; bash launch-local.sh >> ../../log/webapp.log ) &
+(cd . ; bash launch-local.sh >> ../log/data_pipeline.log)
+wait
